030d9c004379163a078bf2532c08c3834f15c77b
Irene890 <munirene20@gmail.com>
2023-07-23 04:25:27 -0700

    Main Header File

--- main.h
+++ main.h
@@ -37,7 +37,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *k,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /************* FUNCTIONS **************/
